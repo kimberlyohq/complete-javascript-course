dd2e87bcd86422b5d66662311472b327573fdec6
kimberly ong <e0424807@u.nus.edu>
2021-06-10 22:31:06 +0800

    Complete Section 3

---
+++ 03-Developer-Skills/starter/codingChallenge.js
@@ -0,0 +1,13 @@
+// Coding Challenge 1
+
+const printForecast = (arr) => {
+    let str = ""
+    for(let i = 0; i < arr.length; i++) {
+       str += `${arr[i]}ºC in ${i + 1} days...`;
+        
+    }
+    console.log(str);
+}
+
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
