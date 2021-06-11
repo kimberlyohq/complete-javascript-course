daea17154932a85b4a7ed4b7a79bcc1b2b7876fa
kimberly ong <e0424807@u.nus.edu>
2021-06-11 21:59:39 +0800

    Complete Project 2

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// Select all the variables you need and store them into variables
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonCloseModal = document.querySelector('.close-modal');
+
+// Returns a node list
+const buttonsShowModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < buttonsShowModal.length; i++) {
+  console.log(buttonsShowModal[i].textContent);
+  buttonsShowModal[i].addEventListener('click', openModal);
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+buttonCloseModal.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
