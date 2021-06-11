f8146bd78a23c42bb5823ed11b892908fe1f1270
kimberly ong <e0424807@u.nus.edu>
2021-06-11 10:20:04 +0800

    Complete Guess My Number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+let message = document.querySelector('.message');
+const number = document.querySelector('.number');
+
+let score = 20;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let guessInput = document.querySelector('.guess');
+console.log(guessInput.value);
+
+if (guessInput.value == number.textContent) {
+  message.textContent = 'CORRECT';
+} else {
+  message.textContent = 'WRONG';
+}
+
+const calcScore = () => {
+  const input = Number(document.querySelector('.guess').value);
+
+  if ((!input || typeof input) !== 'number') {
+    message.textContent = 'Not a valid number';
+  } else if (input === secretNumber) {
+    message.textContent = 'CORRECT';
+    score++;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#ffffff';
+  } else {
+    message.textContent = 'WRONG';
+    score--;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#222';
+  }
+};
+
+const checkButton = document.querySelector('.check');
+checkButton.addEventListener('click', calcScore);
